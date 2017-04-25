20bb9c8b71fb455800b3c03ece5ccc1e868b0ef7
ntdinh1987 <ntdinh1987@gmail.com>
2017-04-25 14:46:06 +0700

    Enable api auth with development_account

--- public/api/index.php
+++ public/api/index.php
@@ -7,22 +7,25 @@
  */
 try {
 
-//    if (!isset($_SERVER['PHP_AUTH_USER'])) {
-//        header('WWW-Authenticate: Basic realm="My Realm"');
-//        header('HTTP/1.0 401 Unauthorized');
-//        echo 'Authen Account is require.';
-//        exit;
-//    } else {
-//        $development_accounts = require_once dirname(__FILE__) . '/../../app/development_account.php';
-//        if (
-//            !array_key_exists($_SERVER['PHP_AUTH_USER'], $development_accounts)
-//            || !$_SERVER['PHP_AUTH_PW'] == $development_accounts[$_SERVER['PHP_AUTH_USER']]['password']
-//        ) {
-//            echo 'Authen Account is wrong. Please config in .env';
-//            exit;
-//        }
-//    }
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="My Realm"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Authen Account is require.';
+        exit;
+    } else {
+        $development_accounts = require_once dirname(__FILE__) . '/../../app/development_account.php';
+        if (
+            !array_key_exists($_SERVER['PHP_AUTH_USER'], $development_accounts)
+            || !$_SERVER['PHP_AUTH_PW'] == $development_accounts[$_SERVER['PHP_AUTH_USER']]['password']
+        ) {
+            echo 'Authen Account is wrong. Please config in .env';
+            exit;
+        }
+    }
 
+    if (config('app.env') == 'production') {
+        exit;
+    }
 
     define('IS_API_CALL', true);
 
