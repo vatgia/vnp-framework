ebf64a0873746bfbb17529054c38cf452626044b
ntdinh1987 <ntdinh1987@gmail.com>
2017-05-25 00:04:29 +0700

    command

---
+++ config/all.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'app' => include 'D:\D\Work\Project\VatGia\vatgia-framework\code/config/app.php',
+    'auth' => include 'D:\D\Work\Project\VatGia\vatgia-framework\code/config/auth.php',
+    'database' => include 'D:\D\Work\Project\VatGia\vatgia-framework\code/config/database.php',
+    'development' => include 'D:\D\Work\Project\VatGia\vatgia-framework\code/config/development.php',
+    'view' => include 'D:\D\Work\Project\VatGia\vatgia-framework\code/config/view.php',
+];
+    
