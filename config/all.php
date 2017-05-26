97d64f887aa8057e9e266072c8849f0bb5b4e9cf
ntdinh1987 <ntdinh1987@gmail.com>
2017-05-26 09:59:46 +0700

    ignore composer.lock

--- config/all.php
+++ config/all.php
@@ -1,9 +1,11 @@
 <?php
+/**
+ * Auto build with command build:config
+ */
 return [
-    'app' => include dirname(__FILE__) . '/app.php',
-'auth' => include dirname(__FILE__) . '/auth.php',
-'database' => include dirname(__FILE__) . '/database.php',
-'development' => include dirname(__FILE__) . '/development.php',
-'view' => include dirname(__FILE__) . '/view.php',
+      'app' => include dirname(__FILE__) . '/app.php',
+      'auth' => include dirname(__FILE__) . '/auth.php',
+      'database' => include dirname(__FILE__) . '/database.php',
+      'development' => include dirname(__FILE__) . '/development.php',
+      'view' => include dirname(__FILE__) . '/view.php',
 ];
-    
