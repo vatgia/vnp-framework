188dfe5332e862c3e5255da78a4896d270c793bb
ntdinh1987 <ntdinh1987@gmail.com>
2017-07-31 23:10:10 +0700

    update framework

--- appview/routes/console.php
+++ appview/routes/console.php
@@ -10,5 +10,6 @@ return [
     \VatGia\Helpers\Console\Commands\ControllerGenerateCommand::class,
     \VatGia\Helpers\Console\Commands\ModelGenerateCommand::class,
     \AppView\Commands\WelcomeCommand::class,
-//    \VatGia\Admin\Commands\AdminModuleGenerateCommand::class,
+    \VatGia\Admin\Commands\AdminModuleGenerateCommand::class,
+    \VatGia\Admin\Commands\AdminPublishCommand::class,
 ];
